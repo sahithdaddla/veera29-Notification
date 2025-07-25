c26b216be011322a519850bffdbb48d7c2a535d8
sahithdaddla <daddlasahith>
2025-07-25 14:47:25 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -77,5 +77,5 @@ app.post('/api/notifications', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://13.60.63.74:${port}`);
+  console.log(`Server running at http://51.20.18.163:${port}`);
 });
